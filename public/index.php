4b461d1e17c1cf580eca2444b45a729ca6efde49
Lucas Gomes <lucasgomes@compbyte.com.br>
2020-10-21 13:06:12 -0300

    v1.0
    Tela de Login e puxando dados do banco.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
-    require_once(dirname(__FILE__, 2) . '/src/config/database.php');
+    require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+    require_once(dirname(__FILE__, 2) . '/src/views/login.php');
 
-    Database::getConnection();
+    
